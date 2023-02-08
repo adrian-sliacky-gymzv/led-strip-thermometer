c85ecf1a22fefc0504734cc8c700e66bd127b343
adrian-sliacky-gymzv <120519032+adrian-sliacky-gymzv@users.noreply.github.com>
2023-02-08 22:41:50 +0100

    Update main.ts

--- main.ts
+++ main.ts
@@ -1,58 +1,17 @@
 // ############################################
 const startY = 0
-const numLeds = 8
+const numLeds = 10
 const ledOverflow = true
 
 const flashColor = NeoPixelColors.Violet
 const stripColor = NeoPixelColors.Yellow
 const flashDelay = 190 // delay in ms
-const buttonDelay = 250 // delay in ms
 
-const buttonsSwapped = false
-const controlsSwapped = false
+const steps = [12, 14, 16, 18, 20, 22, 24, 26, 28, 30]
 
 // ############################################
 
-let reversed = false
-
-const updateScreen = () => {
-    basic.showString(reversed ? "O" : ".")
-}
-
-
-updateScreen()
-
-
-input.onButtonPressed(Button.AB, () => {
-    reversed = !reversed
-    updateScreen()
-})
-
-
-let y = startY
-
-
-let button1 = Button.A
-let button2 = Button.B
-
-
-if (buttonsSwapped) {
-    button1 = Button.B
-    button2 = Button.A
-}
-
-
-let increment = 1
-if (controlsSwapped) {
-    increment = -1
-}
-
-
-const mod = (a: number, b: number) => ledOverflow ? ((a % b) + b) % b : Math.max(Math.min(a, numLeds - 1), 0)
-
-
 const strip = neopixel.create(DigitalPin.P0, numLeds, NeoPixelMode.RGB)
-
 
 for (let i = 0; i < 3; i++) {
     strip.showColor(neopixel.colors(flashColor))
@@ -62,28 +21,22 @@ for (let i = 0; i < 3; i++) {
     basic.pause(flashDelay)
 }
 
-
-loops.everyInterval(buttonDelay, () => {
-    if (input.buttonIsPressed(button1)) {
-        y = mod(y + increment, numLeds)
-    }
-    if (input.buttonIsPressed(button2)) {
-        y = mod(y - increment, numLeds)
-    }
-
-
-    strip.clear()
-    if (reversed) {
-        for (let i = 0; i < numLeds; i++) {
-            if (!(i === y)) {
-                strip.setPixelColor(i, stripColor)
-            }
+const findRange = (n: number) => {
+    for (let i = 0; i < steps.length; i++) {
+        const step = steps[i]
+        if (n <= step) {
+            return i
         }
-    } else {
-        strip.setPixelColor(y, stripColor)
     }
+
+    return steps.length - 1
+}
+
+basic.forever(() => {
+    const temp = Math.floor(input.temperature())
+    strip.clear()
+    strip.setPixelColor(findRange(temp), stripColor)
     strip.show()
 })
-
 
 if (input.buttonIsPressed(Button.AB)) { } // for initializing A + B button in makecode Simulator
