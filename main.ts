947fb8a6d92da2a4b439253ff6f160650c3311a2
adrian-sliacky-gymzv <120519032+adrian-sliacky-gymzv@users.noreply.github.com>
2023-02-09 16:09:41 +0100

    Update main.ts

--- main.ts
+++ main.ts
@@ -1,40 +1,47 @@
 // ############################################
-const numLeds = 10
-
-const flashColor = NeoPixelColors.Violet
-const stripColor = NeoPixelColors.Yellow
-const flashDelay = 190 // delay in ms
-
 const steps = [12, 14, 16, 18, 20, 22, 24, 26, 28, 30]
+
+const flash = true
+const numberOfFlashes = 3
+const flashColor = NeoPixelColors.Violet
+const flashDelay = 190 // delay in ms
 
 // ############################################
 
+const numLeds = steps.length
+
 const strip = neopixel.create(DigitalPin.P0, numLeds, NeoPixelMode.RGB)
 
-for (let i = 0; i < 3; i++) {
-    strip.showColor(neopixel.colors(flashColor))
-    basic.pause(flashDelay)
-    strip.clear()
-    strip.show()
-    basic.pause(flashDelay)
+if (flash) {
+    for (let i = 0; i < numberOfFlashes; i++) {
+        strip.showColor(neopixel.colors(flashColor))
+        basic.pause(flashDelay)
+        strip.clear()
+        strip.show()
+        basic.pause(flashDelay)
+    }
 }
 
 const findRange = (n: number) => {
-    for (let i = 0; i < steps.length; i++) {
+    for (let i = 0; i < numLeds; i++) {
         const step = steps[i]
         if (n < step) {
             return i
         }
     }
-
-    return steps.length - 1
+    return numLeds - 1
 }
 
 basic.forever(() => {
     const temp = Math.floor(input.temperature())
-    let step = findRange(temp)
+    const step = findRange(temp)
+
+    const stripRGB = [Math.round(((255 / numLeds)) * step), 0, (255 - (step * (255 / numLeds)))]
+
     strip.clear()
-    strip.setPixelColor(step, stripColor)
+    for (let i = 0; i < step + 1; i++) {
+        strip.setPixelColor(i, neopixel.rgb(stripRGB[0], stripRGB[1], stripRGB[2]))
+    }
     strip.show()
 })
 
